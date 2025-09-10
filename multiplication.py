e625af1dc7e000432740b4932496bcfccff24fcb
rishinmh <therealrishin@gmail.com>
2025-09-10 02:25:28 -0400

    mulipilcation done

---
+++ multiplication.py
@@ -0,0 +1,6 @@
+num = int(input("Enter a number: "))
+limit = int(input("Enter the limit of the table: "))
+
+print("Multipilication Table of ", num)
+for i in range(1, limit+1):
+    print(num,"x",i,"=",num*i)
