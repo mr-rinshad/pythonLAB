26172832abfc7d12bc6ee47943fd904be10e14e3
Muhammed Rinshad <mrmuhammedrinshad@gmail.com>
2025-10-08 03:06:33 -0400

    Delete count(a).py

--- count(a).py
+++
@@ -1,7 +0,0 @@
-# List of first names
-names = ["Rinshad", "Aisha", "Rahul", "Ananya", "Aman", "Sara"]
-
-# Count occurrences of 'a' (case-insensitive)
-count = sum(name.lower().count('a') for name in names)
-
-print("Total occurrences of 'a':", count)
