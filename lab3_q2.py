491f73d309b706767a9aa0e6df897b29aa385eac
Muhammed Rinshad <mrmuhammedrinshad@gmail.com>
2025-10-23 10:32:58 -0400

    Create lab3_q2.py

---
+++ lab3_q2.py
@@ -0,0 +1,23 @@
+# Program to generate Fibonacci series using t1, t2, t3
+
+n = int(input("Enter the number of terms: "))
+
+# first two terms
+t1 = 0
+t2 = 1
+
+if n <= 0:
+    print("Please enter a positive integer")
+elif n == 1:
+    print("Fibonacci series up to", n, "term:")
+    print(t1)
+else:
+    print("Fibonacci series up to", n, "terms:")
+    print(t1, t2, end=" ")
+    
+    for i in range(2, n):
+        t3 = t1 + t2
+        print(t3, end=" ")
+        # update values
+        t1 = t2
+        t2 = t3
