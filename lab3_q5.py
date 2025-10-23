65ed01440994d7b8993a29e19d62cc4536e1170a
Muhammed Rinshad <mrmuhammedrinshad@gmail.com>
2025-10-23 10:35:06 -0400

    Create lab3_q5.py

---
+++ lab3_q5.py
@@ -0,0 +1,10 @@
+# Program to print the multiplication table of a number
+
+# Read number from user
+n = int(input("Enter a number: "))
+
+print(f"Multiplication table of {n}:")
+
+# Loop from 1 to 10
+for i in range(1, 11):
+    print(f"{n} x {i} = {n * i}")
