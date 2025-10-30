d59eb22bdd8e905290e5a007bed6ce93c1e31448
Muhammed Rinshad <mrmuhammedrinshad@gmail.com>
2025-10-30 09:59:53 -0400

    Create lab4_q1.py

---
+++ lab4_q1.py
@@ -0,0 +1,10 @@
+def simple_interest(p, t, senior):
+    rate = 12 if senior else 10
+    return (p * rate * t) / 100
+
+p = float(input("Enter principal amount: "))
+t = float(input("Enter time (in years): "))
+senior = input("Senior citizen (yes/no): ").lower() == "yes"
+
+si = simple_interest(p, t, senior)
+print("Simple Interest =", si)
